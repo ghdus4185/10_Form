a3709a5c8fe4c8778b608b0c8fd2fe3387c38e1a
ghdus4185 <ckdghdus@naver.com>
2019-10-15 09:33:28 +0900

    모델 생성

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'form.html')
